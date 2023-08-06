a7abe43238bb0b23ce1e9e813115fde2c7f9e96f
EAnthonycarranza <129874785+EAnthonycarranza@users.noreply.github.com>
2023-08-06 10:09:32 -0500

    adding mysql requirements

--- server.js
+++ server.js
@@ -13,11 +13,9 @@ const User = require('./models/User');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const Sequelize = require('sequelize');
+const mysql = require('mysql2');
 
-const sequelize = new Sequelize({
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql'
 });
