52e469c9de1c75bb78c6821a9ca12ce597b78f90
EAnthonycarranza <129874785+EAnthonycarranza@users.noreply.github.com>
2023-08-06 08:57:25 -0500

    Updating username/password errors to add a error message

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -16,18 +16,13 @@ router.post('/login', async (req, res) => {
     // Find the user with the given email in the database
     const user = await User.findOne({ where: { email } });
 
-    // If the user is not found, respond with an error message
-    if (!user) {
-      return res.status(400).json({ message: 'Incorrect email or password' });
-    }
-
     // Check if the provided password matches the hashed password in the database
     const passwordMatch = await bcrypt.compare(password, user.password);
 
-    // If the passwords do not match, respond with an error message
-    if (!passwordMatch) {
-      return res.status(400).json({ message: 'Incorrect email or password' });
-    }
+    // If the usernames and passwords do not match, respond with an error message
+    if (!user || !passwordMatch) {
+      return res.render('login', { errorMessage: 'Incorrect email or password. Please try again!' });
+  }
 
     // After verifying the password, set the user as logged in by saving the user ID in the session
     req.session.userId = user.id; // Set the userId in the session
