1b36a54cafab986bb601aba035895ce5a6356d28
EAnthonycarranza <129874785+EAnthonycarranza@users.noreply.github.com>
2023-08-06 17:58:30 -0500

    Adding PetPhoto functionality route

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const PetPhoto = require('./PetPhoto'); // Import the PetPhoto model
 
 class User extends Model {}
 
@@ -33,5 +34,11 @@ User.init({
 });
 
 // Add any model associations here (e.g., User.hasMany(Post))
+
+// Define a one-to-many relationship between User and PetPhoto
+User.hasMany(PetPhoto, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
 
 module.exports = User;
