ac51afccc8398b50e1c81c34f4eb1fdd0ed2c7a7
EAnthonycarranza <129874785+EAnthonycarranza@users.noreply.github.com>
2023-08-06 20:39:00 -0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,34 +4,7 @@ const PetPhoto = require('./PetPhoto'); // Import the PetPhoto model
 
 class User extends Model {}
 
-User.init({
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true
-    }
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  // Add other properties as needed
-}, {
-  sequelize, // We need to pass the connection instance
-  modelName: 'User', // We need to choose the model name
-});
+
 
 // Add any model associations here (e.g., User.hasMany(Post))
 
