acb72a282fa8215740763690e6c402288b5b1b9d
EAnthonycarranza <129874785+EAnthonycarranza@users.noreply.github.com>
2023-08-07 08:51:30 -0500

    adding file size limit to image < 1mb

--- server.js
+++ server.js
@@ -23,11 +23,12 @@ const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'uploads/')
   },
-filename: function (req, file, cb) {
-  const ext = path.extname(file.originalname);
-  const filename = `${uuid.v4()}${ext}`;
-  cb(null, filename);
-}
+  filename: function (req, file, cb) {
+    // Create a unique filename using the original extension
+    const ext = path.extname(file.originalname);
+    const filename = `${uuid.v4()}${ext}`;
+    cb(null, filename)
+  }
 });
 
 const upload = multer({ storage: storage });
